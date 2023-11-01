81c0e3f19b612310bd35632ec612ae2118917c47
Maakis Thoppae <tmaakis@gmail.com>
2023-11-01 19:16:08 +0000

    working

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace cs8080
+{
+    class Disassembler
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Disassembler8080.Disassembler("invaders");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Done, {ex}");
+            }
+        }
+    }
+}
